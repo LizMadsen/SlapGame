305bdd74884dd04ab4e1486a8d412fc34a1cf96f
Liz Madsen <cyberneticdenim@gmail.com>
2021-10-14 14:59:18 -0600

    first commit

---
+++ game.js
@@ -0,0 +1,45 @@
+let health = 100
+let enemyName = 'Frank'
+let hits = 0
+
+// function slap() {
+//     health--
+//     hits++
+//     update()
+//     console.log(slap)
+// }
+
+// function punch() {
+//     health -= 5
+//     hits++
+//     update()
+//     console.log(punch)
+// }
+
+// function kick() {
+//     health -= 10
+//     hits++
+//     update()
+//     console.log(kick)
+// }
+
+let attackType = {
+    punch: { type: 'Punch', health: 1 },
+    sword: { type: 'Sword', health: 5 },
+    mace: { type: 'Mace', health: 10 },
+}
+
+function attack(atkChoice) {
+    let chosenAtk = attackType[atkChoice]
+    console.log(chosenAtk)
+    health -= chosenAtk.health
+    hits++
+    update()
+}
+
+function update() {
+    let btnElem = document.getElementById('health').innerText = health.toString()
+    document.getElementById('hits').innerText = hits.toString()
+}
+
+update()
